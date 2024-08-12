837d70e188999626049941e9a45dec2dcb9524eb
bhagened <bhageneder@ycp.edu>
2024-08-12 05:11:06 -0400

    Working Data Parsing
    Updated code to include parsing of HTML and storage inside csv

--- WebDataScraper/WebDataScraper.cpp
+++ WebDataScraper/WebDataScraper.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-#include <algorithm>
-#include <cctype>  // for std::isspace
-#include <string>
-#include <curl/curl.h>
-#include "libxml/HTMLparser.h"
-#include "libxml/xpath.h"
+#include <vector>
+#include <fstream>
+#include <curl/curl.h> // for retriving HTML documents
+#include "libxml/HTMLparser.h" // for parsing HTML documents
+#include "libxml/xpath.h" // for organizing parsed info
+
+struct Product {
+    std::string url;
+    std::string image;
+    std::string name;
+    std::string price;
+};
 
 // Callback function used for CURL to process data buffer
 size_t WriteCB(void* contents, size_t size, size_t nmemb, std::string* response) {
@@ -26,7 +32,7 @@ std::string get_request(const std::string& url) {
     std::string result;
 
     if (curl) {
-        // perform the GET request
+        // perform GET request
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L); // Toggle Verbose Debug Level: 0L (none) -> 1L (standard)
 
         // Convert URL to proper CURL string
@@ -44,11 +50,30 @@ std::string get_request(const std::string& url) {
             std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
         }
 
-        // free up the local curl resources
+        // free local curl resources
         curl_easy_cleanup(curl);
     }
     // return un-processed scraped string
     return result;
+}
+
+void* write_to_csv(std::vector<Product> products) {
+    // create the CSV file of output
+    std::ofstream csv_file("products.csv");
+    // populate it with the header
+    csv_file << "url,image,name,price" << std::endl;
+    // populate the CSV output file
+    for (int i = 0; i < products.size(); ++i) {
+        // transform a Product instance to a
+        // CSV string record
+        Product p = products.at(i);
+        std::string csv_record = p.url + "," + p.image + "," + p.name + "," + p.price;
+        csv_file << csv_record << std::endl;
+    }
+    // free up the resources for the CSV file
+    csv_file.close();
+
+    return 0;
 }
 
 int main() {
@@ -62,7 +87,45 @@ int main() {
 
     // scraping logic... Now we need to post-process
 
-    // free up the global curl resources
+    // forward HTML document to libxml2
+    htmlDocPtr doc = htmlReadMemory(html_document.c_str(), html_document.length(), nullptr, nullptr, HTML_PARSE_NOERROR);
+
+    // retrieve all HTML li.product elements with the XPath selector
+    xmlXPathContextPtr context = xmlXPathNewContext(doc);
+    xmlXPathObjectPtr product_html_elements = xmlXPathEvalExpression((xmlChar*)"//li[contains(@class, 'product')]", context);
+
+    // initialize an array of "Product"
+    std::vector<Product> products;
+
+    // Iterate over the list of the product nodes and extract the desired data:
+    for (int i = 0; i < product_html_elements->nodesetval->nodeNr; ++i) {
+        // get the current element of the loop
+        xmlNodePtr product_html_element = product_html_elements->nodesetval->nodeTab[i];
+
+        // set the context to restrict XPath selectors
+        // to the children of the current element
+        xmlXPathSetContextNode(product_html_element, context);
+
+        xmlNodePtr url_html_element = xmlXPathEvalExpression((xmlChar*)".//a", context)->nodesetval->nodeTab[0];
+        std::string url = std::string(reinterpret_cast<char*>(xmlGetProp(url_html_element, (xmlChar*)"href")));
+        xmlNodePtr image_html_element = xmlXPathEvalExpression((xmlChar*)".//a/img", context)->nodesetval->nodeTab[0];
+        std::string image = std::string(reinterpret_cast<char*>(xmlGetProp(image_html_element, (xmlChar*)"src")));
+        xmlNodePtr name_html_element = xmlXPathEvalExpression((xmlChar*)".//a/h2", context)->nodesetval->nodeTab[0];
+        std::string name = std::string(reinterpret_cast<char*>(xmlNodeGetContent(name_html_element)));
+        xmlNodePtr price_html_element = xmlXPathEvalExpression((xmlChar*)".//a/span", context)->nodesetval->nodeTab[0];
+        std::string price = std::string(reinterpret_cast<char*>(xmlNodeGetContent(price_html_element)));
+
+        Product product = { url, image, name, price };
+        products.push_back(product);
+    }
+
+    // free libxml2 resources
+    xmlXPathFreeContext(context);
+    xmlFreeDoc(doc);
+
+    write_to_csv(products);
+
+    // free global curl resources
     curl_global_cleanup();
 
     return 0;
